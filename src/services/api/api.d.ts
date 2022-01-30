811bdd6df0581be8c0221770c46bfba8f92ac5f6
Yury Regis <yury_regis@hotmail.com>
2022-01-30 00:18:32 -0300

    add route: getAllHeroes

--- src/services/api/api.d.ts
+++ src/services/api/api.d.ts
@@ -1,7 +1,48 @@
+
+
 declare module 'api' {
 
+    interface Thumbnail {
+        path: string;
+        extension: string;
+    }
+
+    export interface ComicProps {
+
+    }
+
+    export interface SeriesProps {
+
+    }
+
+    export interface StoriesProps {
+
+    }
+
+    export interface EventsProps {
+
+    }
+
     export interface HeroProps {
-       
+        id: number;
+		name: string;
+		description: string;
+		modified: string;
+        thumbnail: Thumbnail;
+        resourceURI: string;
+        comics: ComicProps;
+        series: SeriesProps;
+        stories: StoriesProps;
+        events: EventsProps;
+        urls: [any];
+    }
+
+    export interface ResponseProps {    
+       offset: number;
+       limit: number;
+       total: number;
+       count: number;
+       results: [HeroProps];
     }
     
 }
--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,15 +1,22 @@
+import { ResponseProps } from 'api'
 import axios, { AxiosInstance, AxiosResponse } from 'axios'
-
+import CryptoJS from 'crypto'
 
 const API: AxiosInstance = axios.create({
     baseURL: 'https://developer.marvel.com/',
 })
 
-
 export const getAllHeroes = async() => {
+    const timeStamp = Number(new Date())
+    const publicKey = 'a1c8cb9003de0daad5bd5b85661aa8c6'
+    const privateKey = 'bcf8c4df391cd74710d33f021b7fc039b51355f0'
+    const hash = CryptoJS.MD5(`${timeStamp}${publicKey}${privateKey}`)
+    console.log(hash)
+    const url = `v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+    
     try {
-        const response: AxiosResponse = await API.get('')
-        const data = Object.keys(response.data.message)
+        const response: AxiosResponse = await API.get(url)
+        const data: ResponseProps = response.data
        
         return data
     } catch (error) {
